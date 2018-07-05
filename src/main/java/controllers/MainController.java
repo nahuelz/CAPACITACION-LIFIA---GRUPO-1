7d67e3ceaaf214451c8d9165719b129ce3eaba89
Nahuel Zanelli <nahuel.zanelli@hotmail.com>
2018-07-05 16:17:58 -0300

    spring config

---
+++ src/main/java/controllers/MainController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="users")
+public class MainController {
+	
+	@RequestMapping(value="/page", method = RequestMethod.GET)
+	public ModelAndView getPage() {
+		ModelAndView view = new ModelAndView("hello");
+		return view;
+	}
+
+}
--- src/main/webapp/index.jsp
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<h2>Hello World!!</h2>
-<h2>Hello World!!</h2>
-<h2>Prueba!!</h2>
-</body>
-</html>
